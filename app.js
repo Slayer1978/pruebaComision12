5f67f30d42af929c72ce9e620742b9a9623b8d2c
Slayer1978 <ulisesarmolla78@gmail.com>
2021-09-09 16:48:09 -0300

    una prueba

---
+++ app.js
@@ -0,0 +1,9 @@
+let nombre = "Ulises";
+/*  git init
+    git config user.name
+    git config user.email
+    git add app.js
+    git commit -m "mensaje"
+    git status
+    git remote add origin https://github.com/Slayer1978/pruebaComision12.git
+*/
